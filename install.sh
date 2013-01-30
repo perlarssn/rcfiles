8eb7de55a300706f4d12b01d86fed56f83ea7854
perlarssn <perlarssn@gmail.com>
2013-01-30 15:17:03 -0800

    Added _Xresources to the install script.

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+mv ~/.vim ~/.vim.backup;
+mv ~/.vimrc ~/.vimrc.backup;
+mv ~/.Xresources ~/.Xresources.backup
 ln -s `pwd`/_vim ~/.vim;
 ln -s `pwd`/_vimrc ~/.vimrc;
+ln -s `pwd`/_Xresources ~/.Xresources;
