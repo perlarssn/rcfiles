0aa5db59dafd5adb37867af9129f07081d6416a4
Per Larsson <per@per.com>
2018-09-06 17:34:23 +0200

    Add nerdtree cloning

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -s `pwd`/_vim ~/.vim;
 ln -s `pwd`/_vimrc ~/.vimrc;
 ln -s `pwd`/_Xresources ~/.Xresources;
 ln -s `pwd`/_bashrc ~/.bashrc;
+git clone https://github.com/scrooloose/nerdtree.git _vim/bundle/nerdtree
