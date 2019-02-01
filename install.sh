94aa30d30b041926ff040ec4fc0bca927fc98ee5
Per Larsson <perlarssn@gmail.com>
2019-02-01 10:15:10 +0100

    Install files to parent directory instead of home directory

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ rm ~/.vim;
 rm ~/.vimrc;
 rm ~/.Xresources;
 rm ~/.bashrc;
-ln -s `pwd`/_vim ~/.vim;
-ln -s `pwd`/_vimrc ~/.vimrc;
-ln -s `pwd`/_Xresources ~/.Xresources;
-ln -s `pwd`/_bashrc ~/.bashrc;
+ln -s `pwd`/_vim ../.vim;
+ln -s `pwd`/_vimrc ../.vimrc;
+ln -s `pwd`/_Xresources ../.Xresources;
+ln -s `pwd`/_bashrc ../.bashrc;
 git clone https://github.com/scrooloose/nerdtree.git _vim/bundle/nerdtree
